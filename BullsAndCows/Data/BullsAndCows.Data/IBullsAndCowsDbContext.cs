95392b781d1605ec4e777cc9b25fcc02e64a14f3
vasilevhr <vasilevhr@gmail.com>
2016-02-15 17:38:06 +0200

    Added IBullsAndCowsDbContext.cs

---
+++ BullsAndCows/Data/BullsAndCows.Data/IBullsAndCowsDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace BullsAndCows.Data
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public interface IBullsAndCowsDbContext
+    {
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        int SaveChanges();
+
+        void Dispose();
+    }
+}

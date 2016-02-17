5a30e0353af0279ac3a1b9b446ef237a7bc98c4b
vasilevhr <vasilevhr@gmail.com>
2016-02-17 17:43:31 +0200

    HighScoreResponseModel.cs is done.

--- BullsAndCows/Web/BullsAndCows.Web.Api/Controllers/ScoresController.cs
+++ BullsAndCows/Web/BullsAndCows.Web.Api/Controllers/ScoresController.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace BullsAndCows.Web.Api.Controllers
+﻿namespace BullsAndCows.Web.Api.Controllers
 {
-    public class ScoresController
+    using Services.Data.Contracts;
+    using System.Linq;
+    using System.Web.Http;
+    using AutoMapper.QueryableExtensions;
+    using Models.HighScore;
+
+    public class ScoresController : ApiController
     {
+        private readonly IHighScoreService highScore;
+
+        public ScoresController(IHighScoreService highScore)
+        {
+            this.highScore = highScore;
+        }
+
+        public IHttpActionResult Get()
+        {
+            var highScoreResult = this.highScore
+                .GetLatest()
+                .ProjectTo<HighScoreResponseModel>()
+                .ToList();
+
+            return this.Ok(highScoreResult);
+        }
     }
 }
---
+++ BullsAndCows/Web/BullsAndCows.Web.Api/Models/HighScore/HighScoreResponseModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BullsAndCows.Web.Api.Models.HighScore
+{
+    using Data.Models;
+    using Infrastructure.Mappings;
+    using System;
+    using AutoMapper;
+
+    public class HighScoreResponseModel : IMapFrom<User>, IHaveCustomMappings
+    {
+        public string Username { get; set; }
+
+        public int Rank { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            Mapper.CreateMap<User, HighScoreResponseModel>()
+                .ForMember(u => u.Username, opts => opts.MapFrom(u => u.Email));
+        }
+    }
+}
